43d8425cdef738a58f2460051081afc97386a85e
Alex-Kse <147931670+Alex-Kse@users.noreply.github.com>
2023-11-14 18:07:51 +0300

    Update main2.py

--- lab4/main2.py
+++ lab4/main2.py
@@ -9,6 +9,6 @@ def task() -> float:
         data1 =[i['score'] for i in data]
         data2 =[i['weight'] for i in data]
         for i in range(0,(len(data1))):
-            data3 += data1[i]/data2[i]
+            data3 += data1[i]*data2[i]
         return (f"{data3:.3f}")
 print(task())
