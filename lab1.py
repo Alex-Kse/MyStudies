884ebc2e2f58cf156c08cd2c4c2075f1fe0e9b0b
Alex-Kse <147931670+Alex-Kse@users.noreply.github.com>
2024-02-13 18:53:03 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,8 +1,16 @@
 class Book:
     """ Базовый класс книги. """
+
     def __init__(self, name: str, author: str):
         self._name = name
         self._author = author
+
+    @property
+    def name(self):
+        return self._name
+    @property
+    def author(self):
+        return self._author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -13,21 +21,42 @@ class Book:
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        super().__init__(name,author)
-        if not isinstance(pages,int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    def __repr__(self):  # заменим repr() на __repr__()
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self._pages})"
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages):
+        if not isinstance(pages, int):
             raise TypeError
-        if pages < 0:
+        if pages < 6:
             raise ValueError
-        self.pages = pages
-
-
+        self._pages = pages
+   
 
 
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        super().__init__(name,author)
-        if not isinstance(duration,(float,int)):
+        super().__init__(name, author)
+        self._duration = duration
+
+    def __repr__(self):  # заменим repr() на __repr__()
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self._duration})"
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration):
+        if not isinstance(duration, float):
             raise TypeError
-        if duration <= 0:
+        if duration <= 1:
             raise ValueError
-        self.duration = duration
+        self._duration = duration
